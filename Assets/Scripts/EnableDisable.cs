842ff8d4eed640f18fe929940d85ae87ebecc416
Kyle Gunter <gunterky@shernet.sheridancollege.ca>
2025-01-30 14:21:01 -0500

    Turned stuff on and off

---
+++ Assets/Scripts/EnableDisable.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableDisable : MonoBehaviour
+{
+    SpriteRenderer sr;
+    EnableDisable ed;
+    public GameObject go;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        ed = gameObject.GetComponent<EnableDisable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            sr.enabled = !sr.enabled;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            go.SetActive(!go.activeInHierarchy);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            ed.enabled = !ed.enabled;
+        }
+    }
+}
