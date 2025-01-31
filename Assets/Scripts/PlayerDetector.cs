fa3691d94fba093819bee449afcc822973e87969
DESKTOP-VADHD4K\gylek <kalewizard.dev@gmail.com>
2025-01-31 08:36:23 -0500

    Finished Press E to Interact task

--- Assets/Scripts/PlayerDetector.cs
+++ Assets/Scripts/PlayerDetector.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerDetector : MonoBehaviour
 {
+    public GameObject popup;
+    public GameObject player;
+    Transform playerTransform;
+
+    public float activationDistance = 1f;
+    public float deactivationDistance = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerTransform = player.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (popup.activeInHierarchy)
+        {
+            if (Vector2.Distance(playerTransform.position, transform.position) > deactivationDistance)
+            {
+                popup.SetActive(false);
+            }
+        } else
+        {
+            if (Vector2.Distance(playerTransform.position, transform.position) < activationDistance)
+            {
+                popup.SetActive(true);
+            }
+        }
     }
 }
